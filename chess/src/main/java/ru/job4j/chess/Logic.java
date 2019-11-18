6b2fa99a5493e41103cb8a9970ccc130023f743e
Mikhail Cherednichenko <28explay@gmail.com>
2019-11-18 16:16:10 +0300

    stash

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -1,46 +1,50 @@
 package ru.job4j.chess;
-
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
 import java.util.Arrays;
 import java.util.Optional;
-
-/**
- * //TODO add comments.
- *
- * @author Petr Arsentev (parsentev@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
 public class Logic {
     private final Figure[] figures = new Figure[32];
     private int index = 0;
-
     public void add(Figure figure) {
         this.figures[this.index++] = figure;
     }
-
     public boolean move(Cell source, Cell dest) {
-        boolean rst = false;
+        boolean rst = true;
         int index = this.findBy(source);
         if (index != -1) {
-            Cell[] steps = this.figures[index].way(source, dest);
-            if (steps.length > 0 && steps[steps.length - 1].equals(dest)) {
-                rst = true;
-                this.figures[index] = this.figures[index].copy(dest);
+            try {
+                this.figures[index].way(source, dest);
+            } catch (IllegalStateException ise) {
+                rst = false;
+            }
+            Cell[] steps = null;
+            if (rst) {
+                steps = this.figures[index].way(source, dest);
+            }
+            if (rst && steps.length > 0 && steps[steps.length - 1].equals(dest)) {
+                for (int i = 0; i < steps.length; i++) {
+                    int j = findBy(steps[i]);
+                    if (j != - 1) {
+                        rst = false;
+                        break;
+                    }
+                }
+                if (rst) {
+                    this.figures[index] = this.figures[index].copy(dest);
+                }
+            } else {
+                rst = false;
             }
         }
         return rst;
     }
-
     public void clean() {
         for (int position = 0; position != this.figures.length; position++) {
             this.figures[position] = null;
         }
         this.index = 0;
     }
-
     private int findBy(Cell cell) {
         int rst = -1;
         for (int index = 0; index != this.figures.length; index++) {
@@ -51,7 +55,6 @@ public class Logic {
         }
         return rst;
     }
-
     @Override
     public String toString() {
         return "Logic{" +
--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -1,50 +1,65 @@
 package ru.job4j.chess.firuges.black;
-
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
-/**
- * @author Petr Arsentev (parsentev@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
+import static java.lang.Math.abs;
 public class BishopBlack implements Figure {
     private final Cell position;
-
     public BishopBlack(final Cell position) {
         this.position = position;
     }
-
     @Override
     public Cell position() {
         return this.position;
     }
-
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
-//        if (!isDiagonal(source, dest)) {
-//            throw new IllegalStateException(
-//                    String.format("Could not way by diagonal from %s to %s", source, dest)
-//            );
-//        }
-//        int size = ...;
-//        Cell[] steps = new Cell[size];
-//        int deltaX = ...;
-//        int deltaY = ...;
-//        for (int index = 0; index < size; index++) {
-//            steps[index] = ...
-//        }
-//        return steps;
+        if (!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not way by diagonal from %s to %s", source, dest)
+            );
+        }
+        int size = abs(source.x - dest.x);
+        Cell[] steps = new Cell[size];
+        int deltaX;
+        int deltaY;
+        if (source.x < dest.x) {
+            deltaX = 1;
+            if (source.y < dest.y) {
+                deltaY = 1;
+            } else {
+                deltaY = -1;
+            }
+        } else {
+            deltaX = -1;
+            if (source.y < dest.y) {
+                deltaY = 1;
+            } else {
+                deltaY = -1;
+            }
+        }
+        steps = this.IntermediatePositions(steps, source, deltaX, deltaY);
+        return steps;
     }
 
+    private Cell[] IntermediatePositions(Cell[] steps, Cell source, int deltaX, int deltaY) {
+        Cell[] cellValue = Cell.values();
+        for (int index = 0; index < steps.length; index++) {
+            for (int i = 0; i < cellValue.length; i++) {
+                if (cellValue[i].x == source.x + ((index + 1) * deltaX) && cellValue[i].y == source.y + ((index + 1) * deltaY)) {
+                    steps[index] = cellValue[i];
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
     public boolean isDiagonal(Cell source, Cell dest) {
-        //TODO check diagonal
-        return false;
+        boolean diagonal = false;
+        if(abs(source.x - dest.x) == abs(source.y - dest.y)) {
+            diagonal = true;
+        }
+        return diagonal;
     }
-
     @Override
     public Figure copy(Cell dest) {
         return new BishopBlack(dest);
